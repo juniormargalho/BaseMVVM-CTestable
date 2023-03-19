5e964dc4a028f4abf2c1186037f9a7b5152e0d35
Junior Margalho <juniormargalho@brq.com>
2023-03-19 16:26:05 -0300

    [01] - configured app and created first view controller

---
+++ BaseMVVM-CTestable/Features/Home/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  BaseMVVM-CTestable
+//
+//  Created by Junior Margalho on 19/03/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
